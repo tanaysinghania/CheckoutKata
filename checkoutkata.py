8f02d8fba8bf448f3bf63b599ca639c4a8963b66
tanaysinghania <76918622+tanaysinghania@users.noreply.github.com>
2023-04-04 23:22:00 +0530

    Add files via upload

---
+++ checkoutkata.py
@@ -0,0 +1,41 @@
+class Checkout:
+    def __init__(self, pricing_rules):
+        self.pricing_rules = pricing_rules
+        self.items = {}
+
+    def scan(self, item):
+        if item in self.items:
+            self.items[item] += 1
+        else:
+            self.items[item] = 1
+
+    def calcTotal(self):
+        total = 0
+        for item, quantity in self.items.items():
+            item_rules = self.pricing_rules.get(item, {})
+            uPrice = item_rules.get('uPrice', 0)
+            sPrice = item_rules.get('sPrice', {})
+            if sPrice and quantity >= sPrice.get('quantity', 0):
+                total += sPrice.get('price', 0) * (quantity // sPrice['quantity'])
+                quantity = quantity % sPrice['quantity']
+            total += uPrice * quantity
+        return total
+
+
+pricing_rules = {
+    'A': {'uPrice': 70, 'sPrice': {'quantity': 3, 'price': 180}},
+    'B': {'uPrice': 50, 'sPrice': {'quantity': 3, 'price': 140}},
+    'C': {'uPrice': 30},
+    'D': {'uPrice': 45},
+}
+
+co = Checkout(pricing_rules)
+
+co.scan('A')
+co.scan('B')
+co.scan('A')
+co.scan('C')
+co.scan('D')
+
+total = co.calcTotal()
+print(f"Total price: {total}")
